4dd1f4c649da65360a5875da6605a2f40859a5aa
AnnaSam48 <littlesamurai4@yahoo.co.uk>
2020-06-16 15:40:22 +0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
+import { ChildComponent} from '../child/child.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, ChildComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
